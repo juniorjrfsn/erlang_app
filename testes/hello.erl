30255c2f13e8729c44967382f722005a587aa170
juniorjrfsn <70738507+juniorjrfsn@users.noreply.github.com>
2023-09-04 00:36:40 -0400

    alterações

---
+++ testes/hello.erl
@@ -0,0 +1,29 @@
+-module(hello).
+-export([main/1,usage/0,alooo/1]).
+
+alooo([Name]) -> io:format("Hello, ~s!~n", [Name]).
+
+main([String]) ->
+    try
+        alooo([String])
+    catch
+        _:_ -> usage()
+    end;
+    main(_) -> usage()
+.
+
+usage() -> io:format("usage: factorial integer\n"), halt(1).
+
+%% Execucao utilizando a funcao main
+%% escript hello.erl "World"
+
+%% Execucao utilizando a funcao diretamente
+%% erl -noshell -s hello alooo "World" -s init stop
+
+% erl
+%
+% 1> c(hello).
+% {ok,hello}
+% 2> hello:world().
+% Hello, world
+% ok
