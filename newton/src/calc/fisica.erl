5bc6900601d52d4609eeef645f7c9fd7c2b37b6f
juniorjrfsn <junior.jrfsn@gmail.com>
2023-09-15 07:00:02 -0400

    Updated `fisica.erl` to use `constanteG` and updated `ini.erl` to use the new functions in `fisica.erl`. (generated by useblackbox.ai )

--- newton/src/calc/fisica.erl
+++ newton/src/calc/fisica.erl
@@ -1,13 +1,50 @@
 % calc
 -module(fisica).
 % -compile(export_all).
--export([forcaN/2, forcaG/3]).
+-export([forcaN/2,constanteG/0, forcaG/3,forcaGdaTerraEdaLua/0,pesoN/2]).
 
 forcaN(Massa,Aceleracao)-> (Massa*Aceleracao).
 
-forcaG(Massa1, Massa2,Distancia) ->
-    N = 9.81,
-    Metro=1,
-    Kg=1,
-    (6.67408 *10-11*N*math:pow(Metro,2)/math:pow(Kg,2))*((Massa1*Massa2)/ math:pow(Distancia,2))
+constanteG()->
+    (
+        ((6.67408*math:pow(10,-11))*(5.972*math:pow(10,24)))
+        /math:pow((6.371*math:pow(10,6)),2)
+    )
 .
+
+forcaG(Massa1, Massa2,Distancia) ->
+    FG = (
+        ( (6.67408 *math:pow(10,-11))* Massa1 * Massa2 )
+        /
+        math:pow(Distancia,2)
+    ),
+    [FG,Massa1, Massa2,Distancia]
+.
+forcaGdaTerraEdaLua() ->
+    (
+        ((6.67408 *math:pow(10,-11))* (5.972*math:pow(10,24)) * (7.36*math:pow(10,22)) )
+        /
+        math:pow((3.84*math:pow(10,8)),2)
+    )
+.
+
+pesoN(Massa, Espace  ) ->
+    Newton = case Espace of
+        "Sol"   -> (Massa * 274.13);
+        "Terra" -> (Massa * 9.819649737724951);
+        "Lua"   -> (Massa * 1.625);
+        "Marte" -> (Massa * 3.72076);
+         _      ->  1
+    end,
+    Lugar = case Espace of
+        "Sol"   -> "no Sol";
+        "Terra" -> "na Terra";
+        "Lua"   -> "na Lua";
+        "Marte" -> "em Marte";
+         _      ->  "..."
+    end,
+    {Newton, Massa, Lugar}
+.
+
+%% TERRA e lua
+%% escript ini.erl 100.0 9.81 80.0 70.0 2.0
--- newton/src/ini.erl
+++ newton/src/ini.erl
@@ -10,8 +10,20 @@ main([Mas,Ace,Mas1,Mas2,Dist]) ->
         Massa1      = list_to_float(Mas1),
         Massa2      = list_to_float(Mas2),
         Distancia   = list_to_float(Dist),
-        FG = fisica:forcaG(Massa1,Massa2,Distancia),
-        io:format("A força gravitacional é de: ~p newtons ~n ", [FG])
+        Fg = fisica:forcaG(Massa1,Massa2,Distancia),
+        io:format("A força gravitacional (forcaG) de Massa1 ~p, Massa2 ~p e Distancia ~p é de: ~p newtons ~n ", [
+                lists:nth(2, Fg)
+                , lists:nth(3, Fg)
+                , lists:nth(4, Fg)
+                , lists:nth(1, Fg)
+            ]
+        ),
+        GravidadeTerra = fisica:constanteG(),
+        io:format("A força gravitacional da terra é de: ~p m/s² ~n ", [GravidadeTerra]),
+        GravidadeTerraLua = fisica:forcaGdaTerraEdaLua(),
+        io:format("A força gravitacional da terra e da Lua é de: ~p N ~n ", [GravidadeTerraLua]),
+        {Newton, Massa, Lugar} = fisica:pesoN(M,"Terra"),
+        io:format("O peso é ~.2f N, para a massa de ~.2f kg ~s.~n", [Newton, Massa, Lugar])
     catch
         _:_ ->
             usage()
@@ -34,3 +46,6 @@ usage() ->
 %% escript ini.erl 100.0 9.81 80.0 70.0 2.0
 %% erl -noshell -sname ini -run ini main 100.0 9.81  -s init stop
 %% erl -noshell -sname ini -run ini main "100.0" "9.81"  -s init stop
+
+%% gravidade
+%% escript ini.erl 100.0 9.81 2000000.0 3000000.0 4.0
