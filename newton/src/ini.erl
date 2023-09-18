4c7ec25f8287a15963fcd8ea767bca2f71486ef8
juniorjrfsn <junior.jrfsn@gmail.com>
2023-09-18 07:33:04 -0400

    The diff is adding comments to the code, specifically instructions on how to run the code. (generated by useblackbox.ai )

--- newton/src/ini.erl
+++ newton/src/ini.erl
@@ -53,13 +53,19 @@ usage() ->
 
 % cd newton
 
-% erlc -pa * calc/fisica.erl
-% erlc  ini.erl calc/fisica.erl -- windows
+% $ erlc -pa * calc/fisica.erl
+% $ erlc  ini.erl calc/fisica.erl -- windows
 
-%% executar -> ini:main(["100.0", "9.81", "2000000.0", "3000000.0", "4.0" ]).
-%% escript ini.erl 100.0 9.81 2000000.0 3000000.0 4.0
-%% erl -noshell -sname ini -run ini main 100.0 9.81 2000000.0 3000000.0 4.0  -s init stop
-%% erl -noshell -sname ini -run ini main "100.0" "9.81" "2000000.0" "3000000.0" "4.0"  -s init stop
+%     executar no interative
+% $ erl
+%    Erlang/OTP 26 [erts-14.0.2] [source] [64-bit] [smp:8:8] [ds:8:8:10] [async-threads:1] [jit:ns]
 
+%     Eshell V14.0.2 (press Ctrl+G to abort, type help(). for help)
+%     1> c("ini"). c("calc/fisica").
+%     2> ini:main(["100.0", "9.81", "2000000.0", "3000000.0", "4.0" ]).
 
-%% escript ini.erl 100.0 9.81 2000000.0 3000000.0 4.0
+% $ escript ini.erl 100.0 9.81 2000000.0 3000000.0 4.0
+% $ erl -noshell -sname ini -run ini main 100.0 9.81 2000000.0 3000000.0 4.0  -s init stop
+% $ erl -noshell -sname ini -run ini main "100.0" "9.81" "2000000.0" "3000000.0" "4.0"  -s init stop
+
+% $ escript ini.erl 100.0 9.81 2000000.0 3000000.0 4.0
